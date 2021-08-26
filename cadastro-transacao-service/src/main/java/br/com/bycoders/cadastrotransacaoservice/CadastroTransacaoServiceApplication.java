4fee6a7aeac1fae78fa0ce2986bc3ac02237287e
jonatascoelho1993 <jonatascoelho@magnasistemas.com.br>
2021-08-25 21:54:52 -0300

    Commit inicial: Projeto criado com Spring Initializr

---
+++ cadastro-transacao-service/src/main/java/br/com/bycoders/cadastrotransacaoservice/CadastroTransacaoServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.bycoders.cadastrotransacaoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroTransacaoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroTransacaoServiceApplication.class, args);
+	}
+
+}
---
+++ cadastro-transacao-service/src/test/java/br/com/bycoders/cadastrotransacaoservice/CadastroTransacaoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bycoders.cadastrotransacaoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroTransacaoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
